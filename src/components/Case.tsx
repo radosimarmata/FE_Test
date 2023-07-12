bc8872e1b33b71b13594558f9712dbbcb7f9434d
radosimarmata27 <simarmatarado@gmail.com>
2023-07-12 23:30:16 +0700

    mantine component

--- src/components/Case.tsx
+++ src/components/Case.tsx
@@ -1,9 +1,3 @@
 export default function Case({ children, ...props }: any) {
-  return (
-    <div className="grid grid-cols-12">
-      <section className="col-span-10 col-start-2" {...props}>
-        {children}
-      </section>
-    </div>
-  );
+  return <section {...props}>{children}</section>;
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,77 @@
 import Case from "../components/Case";
+import reactLogo from "../assets/react.svg";
+import reactLogos from "../assets/logo.png";
+import { PasswordInput, Text, Group, TextInput } from "@mantine/core";
 export default function Login() {
   return (
     <Case>
-      <div className="w-full max-w-lg">
-        <h4 className="text-2xl">I'm Login Page</h4>
-        <p className="text-lg leading-relaxed text-gray-400">
-          You can go to this page because the React router.
-        </p>
-      </div>
+      <section className="h-screen">
+        <div className="h-full">
+          <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+            <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
+              <div className="mb-6 sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                  className="mx-auto h-10 w-auto"
+                  src={reactLogo}
+                  alt="reactjs"
+                />
+              </div>
+
+              <form>
+                <div className="mb-3">
+                <Group position="apart" mb={5}>
+                  <Text
+                    component="label"
+                    htmlFor="username"
+                    size="sm"
+                    weight={500}
+                  >
+                    Username
+                  </Text>
+                </Group>
+                <TextInput
+                  id="username"
+                  placeholder="Username"
+                  required
+                  autoComplete="nope"
+                />
+                </div>
+                <div className="mb-3">
+                  <Group position="apart" mb={5}>
+                    <Text
+                      component="label"
+                      htmlFor="password"
+                      size="sm"
+                      weight={500}
+                    >
+                      Password
+                    </Text>
+                  </Group>
+                  <PasswordInput placeholder="Password" id="password" />
+                </div>
+
+                <div className="grid justify-items-end text-center lg:text-left">
+                  <button
+                    type="button"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+              <img
+                src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                className="w-full"
+                alt="Sample image"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </Case>
   );
 }
